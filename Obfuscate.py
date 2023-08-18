32cd2db50f846d29bef51ca34a3eb23da7413cf2
Eric <eric.xing819@topper.wku.edu>
2023-08-18 14:26:30 -0500

    minor bug fix

--- Obfuscate.py
+++ Obfuscate.py
@@ -22,41 +22,6 @@ def clean(texts):
 
     return ret
 
-def replace_interval(words, interval):
-
-    words[interval[0] : interval[1]] =  ["[MASK]"] * (interval[1] - interval[0])
-    save = []
-
-    if interval[1] > 512:
-        split = interval[1] - 512
-        save = words[ :split]
-        words = words[split: ]
-
-    inputs = replacer_tokenizer(" ".join(words), return_tensors="pt", padding="max_length", truncation=True, max_length=512).to(device)
-    token_ids = replacer_tokenizer.encode(" ".join(words), return_tensors="pt", padding="max_length", truncation=True, max_length=512)
-
-    masked_positions = [idx for idx in range(len(words)) if words[idx] == "[MASK]"]
-
-    outputs = replacer(**inputs)
-
-    predictions = outputs[0]
-    sorted_preds, sorted_idx = predictions[0].sort(dim=-1, descending=True)
-
-    predicted_index = [torch.argmax(predictions[0, i]).item() for i in range(len(predictions[0]))]
-    predicted_token = [replacer_tokenizer.convert_ids_to_tokens([predicted_index[x]])[0] for x in range(len(predictions[0]))]
-    predicted_tokens = [predicted_token[pos] for pos in masked_positions]
-
-    del inputs, token_ids, outputs, predictions, sorted_idx, sorted_preds
-    torch.cuda.empty_cache()
-
-    rep_idx = 0
-    for word_idx in range(len(words)):
-        if words[word_idx] == "[MASK]":
-            words[word_idx] = removePunct(predicted_tokens[rep_idx])
-
-    save.extend(words)
-    return save
-
 
 def main():
     now = datetime.now()
@@ -125,14 +90,8 @@ def main():
 
     data['attribution'] = all
 
-    rev_tags = dict()
-    for tag in tags:
-        rev_tags[tags[tag]] = tag
-
     isValid = lambda index : index >= index >= num_char and index < num_char + num_pos
-    to_char = lambda x: tags[x] if x in tags else x
-    token_and_tag = lambda text: [tup[1] for tup in pos_tag(tokenize(text))]
-    token_tag_join = lambda text: ''.join([to_char(tag) for tag in token_and_tag(text)])
+    to_compressed = lambda tag: tags[tag] if tag in tags else tag
 
     for idx, row in data.iterrows():
 
